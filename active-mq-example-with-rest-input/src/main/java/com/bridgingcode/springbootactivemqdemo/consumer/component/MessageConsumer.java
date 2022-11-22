1b9505852fe5b7af77ff94466c735f6183a3f438
mgumplmayr <max0gum@yahoo.com>
2022-11-22 17:25:44 +0100

    Init mosquitto, camunda runtime

---
+++ active-mq-example-with-rest-input/src/main/java/com/bridgingcode/springbootactivemqdemo/consumer/component/MessageConsumer.java
@@ -0,0 +1,22 @@
+package com.bridgingcode.springbootactivemqdemo.consumer.component;
+
+import com.bridgingcode.springbootactivemqdemo.model.SystemMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author AJ Catambay of Bridging Code
+ *
+ */
+@Component
+public class MessageConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumer.class);
+
+    @JmsListener(destination = "bridgingcode-queue")
+    public void messageListener(SystemMessage systemMessage) {
+        LOGGER.info("Message received! {}", systemMessage);
+    }
+}

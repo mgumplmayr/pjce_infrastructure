41777a79f71c458f61d8e8807cc4536ace8ee47b
mgumplmayr <max0gum@yahoo.com>
2023-01-12 11:10:26 +0100

    added method to correlate message by topic

--- itproj-run-camunda/src/main/java/at/jku/itproj/MQTTReceiver.java
+++ itproj-run-camunda/src/main/java/at/jku/itproj/MQTTReceiver.java
@@ -14,15 +14,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class MQTTReceiver implements JavaDelegate {
     MqttClient mqttClient;
-    String topic = "mytopic";
     String broker = "tcp://localhost:1883";
     String clientId = "JavaSubscribeExample";
     RuntimeService runtimeService;
-    String messageName = "message_received"; //der Name der Message muss mit den Topics abgestimmt werden
+
 
 
     @Override
-    public void execute(final DelegateExecution execution) throws Exception {
+    public void execute(final DelegateExecution execution) throws Exception { //topic name = message name = variable name
         System.out.println("Executing MQTTReceiver");
         MemoryPersistence persistence = new MemoryPersistence();
         mqttClient = new MqttClient(broker, clientId, persistence);
@@ -32,20 +31,19 @@ public class MQTTReceiver implements JavaDelegate {
         mqttClient.setCallback(new MqttCallback() {
             public void messageArrived(String topic, MqttMessage message) throws Exception {
                 String messageReceived = new String(message.getPayload());
-                System.out.println("Message received: " + messageReceived);
-                processMessage(messageReceived);
+                System.out.println("Message received: " + messageReceived+ " on topic: " + topic);
+                processMessage(messageReceived, topic);
             }
             public void deliveryComplete(IMqttDeliveryToken token) {
             }
             public void connectionLost(Throwable cause) {
             }
-
         });
-        mqttClient.subscribe(topic);
+        mqttClient.subscribe("message_received"); //auch möglich: # für alle topics
     }
 
 
-    public void processMessage(String message){
+    public void processMessage(String message, String messageName){
         String processID = message; //bei den MQTT Nachrichten muss man die ProcessID des Vorgangs mitschicken
 
         try {
@@ -57,6 +55,7 @@ public class MQTTReceiver implements JavaDelegate {
                     .processInstanceId(processID)
                     .eventName(messageName).singleResult();
             System.out.println("Event to correlate: "+event);
+            runtimeService.setVariable(processID, messageName, message);
 
             if(event==null){
                 System.out.println("No subscription found with ProcessID: "+processID+"and messageName: "+messageName);

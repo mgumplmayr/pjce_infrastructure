50797e0a93b9c84b80003c215078441077f23dbc
mgumplmayr <max0gum@yahoo.com>
2023-02-10 13:38:00 +0100

    Changed ports

--- itproj-run-camunda/src/main/java/at/jku/itproj/DatabaseConnector.java
+++ itproj-run-camunda/src/main/java/at/jku/itproj/DatabaseConnector.java
@@ -6,7 +6,7 @@ import java.sql.*;
  * */
 public class DatabaseConnector {
     private static Connection connection;
-    private static String url = "jdbc:mariadb://localhost:8883/ceprojekt"; //TODO anpassen
+    private static String url = "jdbc:mariadb://localhost:3306/cpvd_system"; //TODO anpassen
     private static String user = "root";
     private static String password = "admin";
 
@@ -14,21 +14,21 @@ public class DatabaseConnector {
      * @param tableName String to be used as the name of the table to insert the message into
      * @param processID String to be inserted into the database in the processID column
      * @param topic String to be inserted into the database in the topic column
-     * @param content String to be inserted into the database in the content column
+     * @param status String to be inserted into the database in the status column
      * */
-    public static void insertMessage(String tableName, String processID, String topic, String content){
+    public static void insertMessage(String tableName, String processID, String topic, String status){
         try {
             //get connection to database
             connection = DriverManager.getConnection(url, user, password);
             System.out.println("Connection to Database successfull");
 
             //build statement
-            String query = "INSERT INTO tableName (process_id, topic, content) VALUES (?, ?, ?)";
+            String query = "INSERT INTO tableName (process_id, topic, status) VALUES (?, ?, ?)";
             query =query.replace("tableName",tableName);
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setString(1, processID);
             statement.setString(2, topic);
-            statement.setString(3, content);
+            statement.setString(3, status);
 
             //execute statement
             int rowsInserted = statement.executeUpdate();
@@ -59,8 +59,8 @@ public class DatabaseConnector {
             while (resultSet.next()) {
                 String date = resultSet.getTimestamp("date").toString();
                 String topic = resultSet.getString("topic");
-                String content = resultSet.getString("content");
-                System.out.println("date: " + date + ", topic: " + topic + ", content: " + content);
+                String status = resultSet.getString("status");
+                System.out.println("date: " + date + ", topic: " + topic + ", status: " + status);
             }
             connection.close();
 
